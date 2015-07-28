8e7301ae1f72a28696bb645382c664aff41c2383
Cyprien IIIAAA <cyprien@iiiaaa.fr>
2015-07-28 14:38:38 +0200

    refonte menu + responsive navbar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 jQuery(document).ready(function($) {
-  $('#tags').inputTags({
-    tags: ['jQuery', 'tags', 'plugin', 'Javascript'],
-    max: 8
-  });
+
+	/* inputTags plugin initialization */
+	$('#tags').inputTags({
+		tags: ['jQuery', 'tags', 'plugin', 'Javascript'],
+		max: 8
+	});
+
+
+	var $overlay 	= $('#overlay');
+	var $trigger 	= $('#mobile-menu-trigger');
+	var $right_menu = $('#right-menu');
+
+	$trigger.on('click', function() {
+		$overlay.fadeToggle().on('click', function() {
+			$overlay.fadeOut();
+			$right_menu.removeClass('is-active');
+		});
+		$right_menu.toggleClass('is-active');
+	});
 });
